efeabd041b021501ac39519cdb67e8c46ef69c0a
Kaylode Kieran <official_kaylode@users.noreply.github.com>
2020-12-15 02:51:19 +0700

    cchange normal unet

--- models/unet.py
+++ models/unet.py
@@ -164,90 +164,82 @@ class AtrousConv(nn.Module):
     def forward(self, x):
         return self.atrous_conv(x)
     
+
 class UNetSemantic(nn.Module):
     """
-    Residual Recuurent Block with attention Unet
-    Implementation : https://github.com/LeeJunHyun/Image_Segmentation
+    UNet - Basic Implementation
+    Paper : https://arxiv.org/abs/1505.04597
     """
-    def __init__(self, in_ch=3, out_ch=1, t=2):
+    def __init__(self, in_ch=3, out_ch=1):
         super(UNetSemantic, self).__init__()
 
-        n1 = 32
+        n1 = 64
         filters = [n1, n1 * 2, n1 * 4, n1 * 8, n1 * 16]
-
+        
         self.Maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.Maxpool2 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.Maxpool3 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.Maxpool4 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.RRCNN1 = RRCNN_block(in_ch, filters[0], t=t)
-        self.RRCNN2 = RRCNN_block(filters[0], filters[1], t=t)
-        self.RRCNN3 = RRCNN_block(filters[1], filters[2], t=t)
-        self.RRCNN4 = RRCNN_block(filters[2], filters[3], t=t)
-        self.RRCNN5 = RRCNN_block(filters[3], filters[4], t=t)
+        self.Conv1 = conv_block(in_ch, filters[0])
+        self.Conv2 = conv_block(filters[0], filters[1])
+        self.Conv3 = conv_block(filters[1], filters[2])
+        self.Conv4 = conv_block(filters[2], filters[3])
+        self.Conv5 = conv_block(filters[3], filters[4])
 
         self.Up5 = up_conv(filters[4], filters[3])
-        self.Att5 = Attention_block(F_g=filters[3], F_l=filters[3], F_int=filters[2])
-        self.Up_RRCNN5 = RRCNN_block(filters[4], filters[3], t=t)
+        self.Up_conv5 = conv_block(filters[4], filters[3])
 
         self.Up4 = up_conv(filters[3], filters[2])
-        self.Att4 = Attention_block(F_g=filters[2], F_l=filters[2], F_int=filters[1])
-        self.Up_RRCNN4 = RRCNN_block(filters[3], filters[2], t=t)
+        self.Up_conv4 = conv_block(filters[3], filters[2])
 
         self.Up3 = up_conv(filters[2], filters[1])
-        self.Att3 = Attention_block(F_g=filters[1], F_l=filters[1], F_int=filters[0])
-        self.Up_RRCNN3 = RRCNN_block(filters[2], filters[1], t=t)
+        self.Up_conv3 = conv_block(filters[2], filters[1])
 
         self.Up2 = up_conv(filters[1], filters[0])
-        self.Att2 = Attention_block(F_g=filters[0], F_l=filters[0], F_int=32)
-        self.Up_RRCNN2 = RRCNN_block(filters[1], filters[0], t=t)
+        self.Up_conv2 = conv_block(filters[1], filters[0])
 
         self.Conv = nn.Conv2d(filters[0], out_ch, kernel_size=1, stride=1, padding=0)
-        
         self.se1 = SE_Block(filters[0])
         self.se2 = SE_Block(filters[1])
         self.se3 = SE_Block(filters[2])
         self.active = torch.nn.Sigmoid()
 
+    def forward(self, x):
 
-    def forward(self, img):
-        
-        e1 = self.RRCNN1(img)
+        e1 = self.Conv1(x)
         e1 = self.se1(e1)
-        
+
         e2 = self.Maxpool1(e1)
-        e2 = self.RRCNN2(e2)
+        e2 = self.Conv2(e2)
         e2 = self.se2(e2)
 
         e3 = self.Maxpool2(e2)
-        e3 = self.RRCNN3(e3)
+        e3 = self.Conv3(e3)
         e3 = self.se3(e3)
 
         e4 = self.Maxpool3(e3)
-        e4 = self.RRCNN4(e4)
+        e4 = self.Conv4(e4)
 
         e5 = self.Maxpool4(e4)
-        e5 = self.RRCNN5(e5)
-        
+        e5 = self.Conv5(e5)
+
         d5 = self.Up5(e5)
-        e4 = self.Att5(g=d5, x=e4)
         d5 = torch.cat((e4, d5), dim=1)
-        d5 = self.Up_RRCNN5(d5)
+
+        d5 = self.Up_conv5(d5)
 
         d4 = self.Up4(d5)
-        e3 = self.Att4(g=d4, x=e3)
         d4 = torch.cat((e3, d4), dim=1)
-        d4 = self.Up_RRCNN4(d4)
+        d4 = self.Up_conv4(d4)
 
         d3 = self.Up3(d4)
-        e2 = self.Att3(g=d3, x=e2)
         d3 = torch.cat((e2, d3), dim=1)
-        d3 = self.Up_RRCNN3(d3)
+        d3 = self.Up_conv3(d3)
 
         d2 = self.Up2(d3)
-        e1 = self.Att2(g=d2, x=e1)
         d2 = torch.cat((e1, d2), dim=1)
-        d2 = self.Up_RRCNN2(d2)
+        d2 = self.Up_conv2(d2)
 
         out = self.Conv(d2)
 
